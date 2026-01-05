78b84fd487c8f6cdd95117624e4b9afa47d2ad32
kamariza30 <kamarizasuhaini@gmail.com>
2026-01-06 01:23:24 +0800

    Code is complete, just need to be edit for submission

--- terrafform/ec2.tf
+++ terrafform/ec2.tf
@@ -48,6 +48,13 @@ resource "aws_security_group" "private_sg" {
      protocol    = "tcp"
      cidr_blocks = ["10.0.0.0/24"]
     }
+
+    ingress {
+      from_port   = 9090
+      to_port     = 9090
+      protocol    = "tcp"
+      cidr_blocks = ["10.0.0.0/24"]
+    }
     egress {
       from_port   = 0
       to_port     = 0
