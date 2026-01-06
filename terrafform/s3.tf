025b8b7d5f3d580a23b6438bed5bd439e09ed4dc
kamariza30 <kamarizasuhaini@gmail.com>
2026-01-06 16:34:19 +0800

    triggerng git clone

--- terrafform/s3.tf
+++ terrafform/s3.tf
@@ -1,6 +1,6 @@
 #below code is to create s3 bucket with versioning enabled
 resource "aws_s3_bucket" "final_project_bucket" {
-  bucket = "devops-bootcamp-final-project-kamariza2" #bucket name must be globally unique
+  bucket = "devops-bootcamp-final-project-kamariza3" #bucket name must be globally unique
   force_destroy = true #to allow terraform to delete non-empty bucket
     
     tags = {
